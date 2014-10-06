0e919fcc73ad5ff2b0f3b0601055e4402d9624bd
WhatsARanjit <whatsaranjit@gmail.com>
2014-10-06 15:14:29 -0400

    Ignorinc pyc, adding some debug

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -14,4 +14,30 @@ def MainMenu():
 
     oc = ObjectContainer()
 
+    oc.add(DirectoryObject(key=Callback(InCinema), title=L('incinema')))
+
+    return oc
+
+##########################################################################
+@route('/video/movietube/incinema', allow_sync=True)
+def InCinema():
+
+    oc = ObjectContainer(title2=('incinema'))
+    req = HTTP.Request(MOVIES_CINEMA, values=dict(
+        c = 'song',
+        a = 'retrieve',
+        p = '{"Page":"1","NextToken":"","VideoYoutubeType":"English","Genere":"","Year":"","Sortby":"Score"}'
+    ))
+    page = req.content
+    Log(page)
+
+    for result in page.xpath("//*[@id='list_content']/table[@class='list_table']/tbody//tr"):
+
+        oc.add(EpisodeObject(
+            #url = MOVIES_HOST + result.xpath("*//a")[0].get('href')
+            #title = result.xpath("*//a/text()")[0]
+            url = 'http://www.movietubenow.com/watch.php?v=4xGNPihcAf8',
+            title = 'This Is Where I Leave You (2014)'
+        ))
+
     return oc
