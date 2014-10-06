b7a93f1857052b0b0aa7a1face033f33c1298321
WhatsARanjit <whatsaranjit@gmail.com>
2014-10-06 19:11:52 -0400

    Getting a listing, but need to adjust player

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,10 +1,16 @@
+import re
+
 NAME = 'MovieTube'
 MOVIES_HOST = 'http://www.movietubenow.com'
 MOVEIES_CINEMA = '%s/search.php?QuickSelectType=4|Score' % MOVIES_HOST
 MOVIES_SEARCH = '%s/index.php' % MOVIES_HOST
 
 HTTP_HEADERS = {'User-Agent': 'Mozilla/5.0 (iPad; CPU OS 6_0_1 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A523 Safari/8536.25'}
-RE_SEARCH = Regex('(<tr.*</tr>$)')
+RE_SEARCH = r'<tr.+?>.*?</tr>'
+RE_ID = r'watch\.php\?v=([a-zA-Z0-9]+)'
+RE_URL = r'<a.+?>(.*?)</a>'
+RE_THUMB = r'src="(.+?)"'
+RE_SUMMARY = r'<h3_light class="text"></h3_light><br/><h3_light class="text">(.+?)</h3_light>'
 
 ##########################################################################
 def Start():
@@ -26,27 +32,37 @@ def MainMenu():
 @route('/video/movietube/incinema')
 def InCinema():
 
-    Log("TEST")
     oc = ObjectContainer(title2=L('incinema'))
-    req = HTTP.Request(MOVIES_SEARCH, values=dict(
+    req = HTTP.Request(MOVIES_SEARCH, headers=HTTP_HEADERS, values=dict(
         c='song',
         a='retrieve',
         p='{"Page":"1","NextToken":"","VideoYoutubeType":"English","Genere":"","Year":"","Sortby":"Score"}'
     ))
-    #page = RE_SEARCH.search(req.content)[0]
+    results = re.findall(RE_SEARCH, req.content)
 
-    oc.add(EpisodeObject(
-        url = 'http://www.movietubenow.com/watch.php?v=4xGNPihcAf8',
-        title = 'This Is Where I Leave You (2014)'
-    ))
+    #oc.add(EpisodeObject(
+    #    url = 'http://www.movietubenow.com/watch.php?v=4xGNPihcAf8',
+    #    title = 'This Is Where I Leave You (2014)',
+    #    summary = results[0]
+    #))
 
-    #for result in page.xpath("//*[@id='list_content']/table[@class='list_table']/tbody//tr"):
+    for result in results:
 
-    #    oc.add(EpisodeObject(
-    #        #url = MOVIES_HOST + result.xpath("*//a")[0].get('href')
-    #        #title = result.xpath("*//a/text()")[0]
-    #        url = 'http://www.movietubenow.com/watch.php?v=4xGNPihcAf8',
-    #        title = 'This Is Where I Leave You (2014)'
-    #    ))
+        try:
+            id = re.findall(RE_ID, result) 
+            url = MOVIES_HOST + '/watch.php?v=' + id[0]
+            title = re.findall(RE_URL, result)
+            thumb = re.findall(RE_THUMB, title[0])
+            summary = re.findall(RE_SUMMARY, result)
+
+            oc.add(EpisodeObject(
+                url = url,
+                title = title[1],
+                thumb = thumb[0],
+                summary = summary[1],
+            ))
+        except:
+            #raise Ex.MediaNotAvailable
+            Log(id)
 
     return oc
