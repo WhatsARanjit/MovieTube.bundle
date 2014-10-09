268ac3d8befaad0857c5505dd57e57a45cebbabf
WhatsARanjit <whatsaranjit@gmail.com>
2014-10-09 12:59:56 -0400

    Need a root items node in XML

--- Contents/bin/get_list.py
+++ Contents/bin/get_list.py
@@ -40,6 +40,7 @@ except Exception as error:
 else:
     print "=> Opening file..."
     f = open('incinema.xml', 'w')
+    f.write("<?xml version=\"1.0\"?>\n")
     
     for result in results:
         # Try finding all the elements
@@ -96,7 +97,7 @@ else:
                 video_url = video
                 source = 'None'
 
-            data = "<item>\n\t<title>%s</title>\n\t<summary>%s</summary>\n\t<thumb>%s</thumb>\n\t<video_url>%s</video_url>\n\t<source>%s</source>\n</item>\n" % (title[1], summary[1], thumb[0], video_url, source)
+            data = "\t<item>\n\t\t<title>%s</title>\n\t\t<summary>%s</summary>\n\t\t<thumb>%s</thumb>\n\t\t<video_url>%s</video_url>\n\t<\tsource>%s</source>\n\t</item>\n" % (title[1], summary[1], thumb[0], video_url, source)
             print "==> Writing data to XML file..."
             f.write(data)
 
