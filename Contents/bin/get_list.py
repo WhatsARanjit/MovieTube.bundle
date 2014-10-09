e920e76b9a3df0d4adb95745f614ca4780406b61
WhatsARanjit <whatsaranjit@gmail.com>
2014-10-09 09:36:32 -0400

    Moved around try/excepts.  Looking for Google Doc URL from main page.

--- Contents/bin/get_list.py
+++ Contents/bin/get_list.py
@@ -16,38 +16,75 @@ RE_ID = r'watch\.php\?v=([a-zA-Z0-9]+)'
 RE_URL = r'<a.+?>(.*?)</a>'
 RE_THUMB = r'src="(.+?)"'
 RE_SUMMARY = r'<h3_light class="text"></h3_light><br/><h3_light class="text">(.+?)</h3_light>'
+RE_DOCS = r'(?:content|src)="(https://docs\.google\.com/file/.*?/preview)">'
 
+# Trying pulling the URL
 try:
-    incinema = urllib.urlopen(MOVIES_SEARCH, INCINEMA_POST)
+    print "=> Accessing search URL..."
+    incinema = urllib.urlopen(MOVIES_SEARCH, INCINEMA_POST, HTTP_HEADERS)
     results = re.findall(RE_SEARCH, incinema.read())
-    f = open('movietube.xml', 'w')
 
-    try:
-        for result in results:
+except Exception as error:
+    print "Could not request search URL"
+    print type(error)
+    print error.args
+    print error
+
+else:
+    print "=> Opening file..."
+    f = open('incinema.xml', 'w')
+
+    for result in results:
+        # Try finding all the elements
+        try:
             id = re.findall(RE_ID, result) 
             url = MOVIES_HOST + '/watch.php?v=' + id[0]
             title = re.findall(RE_URL, result)
             thumb = re.findall(RE_THUMB, title[0])
             summary = re.findall(RE_SUMMARY, result)
+            print "==> Finding metadata for " + title[1] + "..."
 
+            print "==> Requesting URL page..."
             params = urllib.urlencode({
                 'c':'result',
                 'a':'getplayerinfo',
                 'p':'{"KeyWord":"' + id[0] + '"}'
             })
-            video = urllib.urlopen(MOVIES_SEARCH, params).read()
+            video = urllib.urlopen(MOVIES_SEARCH, params, HTTP_HEADERS).read()
 
-            video_url = video.find('vjplayer')
-            print video_url
+            # See if there is a Google Docs URL
+            try:
+                gdocs = re.findall(RE_DOCS, video)[0]
+            except:
+                gdocs = ''
 
-            data = "<item>\n\t<title>%s</title>\n\t<summary>%s</summary>\n\t<thumb>%s</thumb>\n\t<video_url>%s</video_url>\n</item>\n" % (title[1], summary[1], thumb[0], video)
+        except IndexError as error:
+            print id
+
+        except Exception as error:
+            print "Could not POST for clip information"
+            print type(error)
+            print error.args
+            print error
+
+        else:
+            print "==> Setting video_url..."
+            if gdocs:
+                video_url = gdocs
+            elif video.find('vjplayer') > -1:
+                expr = r'src="(.*?)"'
+                video_url = re.findall(expr, urllib.unquote(video).decode('utf8'))[0]
+            elif video.find('vkplayer') > -1:
+                expr = r'data="(.*?)"'
+                video_url = re.findall(expr, urllib.unquote(video).decode('utf8'))[0]
+            elif video.find('uplayer') > -1:
+                expr = r'src="(.*?)"'
+                video_url = re.findall(expr, urllib.unquote(video).decode('utf8'))[0]
+            else:
+                video_url = video
+
+            data = "<item>\n\t<title>%s</title>\n\t<summary>%s</summary>\n\t<thumb>%s</thumb>\n\t<video_url>%s</video_url>\n</item>\n" % (title[1], summary[1], thumb[0], video_url)
+            print "==> Writing data to XML file..."
             f.write(data)
-    except:
-        print id
 
     f.close()
-
-except Exception as error:
-    print type(error)
-    print error.args
-    print error
